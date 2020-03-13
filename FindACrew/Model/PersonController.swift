3bd3cac8a86465b507b281fa1a8424cac973abcb
Matt Martindale <martindalematt18@yahoo.com>
2020-03-12 21:41:56 -0600

    finished FindACrew

--- FindACrew/Model/PersonController.swift
+++ FindACrew/Model/PersonController.swift
@@ -7,3 +7,53 @@
 //
 
 import Foundation
+
+class PersonController {
+    
+    enum HTTPMethod: String {
+        case get = "GET"
+        case post = "POST"
+        case put = "PUT"
+        case delete = "DELETE"
+    }
+    
+    private let baseURL = URL(string: "https://swapi.co/api/people/")!
+    
+    func searchForPeopleWith(searchTerm: String, completion: @escaping ([Person]) -> Void) {
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        let searchTermQueryItem = URLQueryItem(name: "search", value: searchTerm)
+        urlComponents?.queryItems = [searchTermQueryItem]
+        
+        guard let requestURL = urlComponents?.url else {
+            print("Error: Request URL is nil")
+            completion([Person]())
+            return
+        }
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            guard error == nil else {
+                print("Error fetching data: \(error!)")
+                completion([])
+                return
+            }
+            
+            guard let data = data else {
+                print("Error: no data returned from data task.")
+                completion([])
+                return
+            }
+            
+            let jsonDecoder = JSONDecoder()
+            do {
+                let personSearch = try jsonDecoder.decode(PersonSearch.self, from: data)
+                completion(personSearch.results)
+            } catch {
+                print("Unable to decode data: \(error)")
+                completion([])
+            }
+        }.resume()
+    }
+}
--- FindACrew/View/PersonTableViewCell.swift
+++ FindACrew/View/PersonTableViewCell.swift
@@ -10,10 +10,22 @@ import UIKit
 
 class PersonTableViewCell: UITableViewCell {
     
+    var person: Person? {
+        didSet {
+            updateViews()
+        }
+    }
+    
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var genderLabel: UILabel!
     @IBOutlet weak var yearLabel: UILabel!
     
-    
+    private func updateViews() {
+        guard let person = person else { return }
+        
+        nameLabel.text = person.name
+        genderLabel.text = person.gender
+        yearLabel.text = person.birthYear
+    }
 
 }
